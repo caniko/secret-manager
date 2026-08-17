3dcad57d4076be9c5ebcf0822d1ba1444e6ef132
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-17 11:14:16 +0200

    chore: bump rs-harbor pin to 77d0a93 (dedup overlapping cross targets)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Declarative secret management for NixOS — resolve and render age/agenix secrets in Rust";
 
   inputs = {
-    rs-harbor.url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=f209ddbca3fdbb0dc31fa3886ccc2ff7369c18ac";
+    rs-harbor.url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=77d0a937c760e6ced8b7ec8fc5a214f550abe35e";
 
     nixpkgs.follows = "rs-harbor/nixpkgs";
     rust-overlay.follows = "rs-harbor/rust-overlay";
